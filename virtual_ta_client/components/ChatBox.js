40a27b82f25edda1f32c46ce6f3fdb960eb48690
Madhur20 <59988556+Madhur20@users.noreply.github.com>
2022-10-20 19:23:10 -0500

    ChatBoxCleanUp

--- virtual_ta_client/components/ChatBox.js
+++ virtual_ta_client/components/ChatBox.js
@@ -1,8 +1,63 @@
+import * as React from 'react';
 import {Box} from "@mui/material";
-import TextInputBar from "./TextInputBar";
+import TextField from '@mui/material/TextField';
 import Typography from "@mui/material";
+import axios from "axios";
 
 export default function ChatBox() {
+
+    const [question,setQuestion] = React.useState("");
+    const [isSuper, setIsSuper] = React.useState(false);
+
+    const handleChange = (e) => {
+        console.log(e);
+        const val = e.target.value;
+        
+        if(val.charAt(val.length-1) === "^"){
+            if(isSuper){
+                setIsSuper(false);
+            }
+            else{
+                setIsSuper(true);
+            }
+            setQuestion(val.substring(0,val.length-1));
+        }
+        else{
+            if(isSuper){
+                
+                const toSuper = val[val.length-1];
+                const tryd = <Text style={{fontsize: 15, lineHeight: 18}} > {toSuper} </Text>
+                console.log(tryd);
+                const toAppend = val.substring(0,val.length-1) + tryd.props.children[1];
+                setQuestion(toAppend);
+              
+            }
+            else{
+                setQuestion(e.target.value);
+            }
+        }
+        // console.log(isSuper);
+    }
+    const sendChange = (e) => {
+        // console.log(e.keyCode);
+        if(e.keyCode == 39){
+            setIsSuper(false);
+        }
+        
+        if(e.keyCode == 13){
+            console.log(question);
+            //await axios.post("/localhost:8080/userQuery", question);
+            //await fetch("/localhost:8080/sendAnswer")
+            //.then(res => res.json())
+            //.then((jsonRes) => {
+            //    setAnswer(jsonRes);
+            //})
+            //.catch((e) => {
+            //     console.log(e);
+            // })
+            setQuestion("");
+        }
+    }
 
     return(
         <div style={{height:"85vh", width:"100%"}}>
@@ -66,7 +121,23 @@ export default function ChatBox() {
                     Scrollable Chat
                 </Box>
                 
-                <TextInputBar />
+                <Box
+                    sx={{
+                        width:"100%",
+                        border:"1px solid lightblue",
+                        borderRadius:"10px",
+                    }}
+                    >
+                    <TextField 
+                        id="text-bar"
+                        label="Ask Something..." 
+                        variant="outlined" 
+                        fullWidth
+                        value={question}
+                        onChange={handleChange}
+                        onKeyDown={sendChange}
+                    />
+                </Box>
             </Box>
             </Box>
         </div>
