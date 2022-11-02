73da8c4b41f9e98327757a9b57b7cbe344c19e28
Madhur20 <59988556+Madhur20@users.noreply.github.com>
2022-11-02 02:48:20 -0500

    calc

--- virtual_ta_client/components/ChatBox.js
+++ virtual_ta_client/components/ChatBox.js
@@ -1,180 +1,169 @@
-import * as React from 'react';
-import {Box} from "@mui/material";
-import TextField from '@mui/material/TextField';
+import * as React from "react";
+import { Box } from "@mui/material";
+import TextField from "@mui/material/TextField";
 import Typography from "@mui/material";
 import axios from "axios";
 import ScrollableChat from "./ScrollableChat";
-// import EquationEditor from "equation-editor-react";
-// import 'mathquill/build/mathquill.css';
+import "katex/dist/katex.min.css";
+
+var Latex = require("react-latex-next");
 
 export default function ChatBox() {
+  const messageType = [
+    {
+      id: String,
+      msg: String,
+    },
+  ];
 
-    const messageType = [{
-        id: String,
-        msg: String,
-    }]
+  const [question, setQuestion] = React.useState("");
+  const [isSuper, setIsSuper] = React.useState(false);
+  const [answer, setAnswer] = React.useState("");
+  const [messages, setMessages] = React.useState([]);
+  const [latex, setLatex] = React.useState([]);
 
-    const [question,setQuestion] = React.useState("");
-    const [isSuper, setIsSuper] = React.useState(false);
-    const [answer, setAnswer] = React.useState("");
-    const [messages, setMessages] = React.useState([]);
+  const handleChange = (e) => {
+    // console.log(e);
+    const val = e.target.value;
 
+    setQuestion(e.target.value);
+  };
+  const sendChange = async (e) => {
+    if (e.keyCode == 13) {
+      // console.log(question);
+      const ques = {
+        id: "fromUser",
+        msg: question,
+      };
+      setMessages((messages) => messages.concat(ques));
+      // setMessages([...messages, ques]);
+      //await axios.post("http://localhost:5000/userQuery", question);
+      const ans = await fetch("http://localhost:8080/sendAnswer/" + question)
+        .then((res) => res.json())
+        .then((jsonRes) => {
+          return jsonRes.answer;
+          //console.log(jsonRes.answer);
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+      setAnswer(ans);
+      const data = {
+        id: "fromBot",
+        msg: ans,
+      };
 
-    const handleChange = (e) => {
-        // console.log(e);
-        const val = e.target.value;
-        
-        if(val.charAt(val.length-1) === "^"){
-            if(isSuper){
-                setIsSuper(false);
-            }
-            else{
-                setIsSuper(true);
-            }
-            setQuestion(val.substring(0,val.length-1));
-        }
-        else{
-            if(isSuper){
-                
-                const toSuper = val[val.length-1];
-                const tryd = <Text style={{fontsize: 15, lineHeight: 18}} > {toSuper} </Text>
-                console.log(tryd);
-                const toAppend = val.substring(0,val.length-1) + tryd.props.children[1];
-                setQuestion(toAppend);
-              
-            }
-            else{
-                setQuestion(e.target.value);
-            }
-        }
-        // console.log(isSuper);
+      setMessages((messages) => messages.concat(data));
+
+      // setMessages([...messages, data]);
+      setQuestion("");
     }
-    const sendChange = async (e) => {
-        // console.log(e.keyCode);
-        if(e.keyCode == 39){
-            setIsSuper(false);
-        }
-        
-        if(e.keyCode == 13){
-            // console.log(question);
-            const ques = {
-                id: "fromUser",
-                msg: question,
-            }
-            setMessages(messages => messages.concat(ques));
-            // setMessages([...messages, ques]);
-            //await axios.post("http://localhost:5000/userQuery", question);
-            const ans = await fetch("http://localhost:8080/sendAnswer/"+ question)
-                        .then(res => res.json())
-                        .then((jsonRes) => {
-                            
-                            return jsonRes.answer;
-                            //console.log(jsonRes.answer);
-                        })
-                        .catch((e) => {
-                            console.log(e);
-                        })
-            setAnswer(ans);
-            const data = {
-                id: "fromBot",
-                msg: ans,
-            }
+  };
 
-            setMessages(messages => messages.concat(data));
+  const l = `$$\\frac_{1}{2}$$`;
 
-            // setMessages([...messages, data]);
-            setQuestion("");
-        }
-        
-    }
+  return (
+    <div style={{ height: "90vh", width: "100%" }}>
+      <Box
+        sx={{
+          height: "90%",
+          backgroundColor: "pink",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          color: "black",
+          marginTop: "3%",
+          marginBottom: "10%",
+          marginLeft: "20%",
+          marginRight: "20%",
+          padding: "10px",
+          borderRadius: "15px",
+        }}
+      >
+        <Box
+          sx={{
+            width: "100%",
 
-    return(
-        <div style={{height:"90vh", width:"100%"}}>
-            <Box sx={{
-                height:"90%",
-                backgroundColor:"pink", 
-                display:"flex",
-                flexDirection:"column",
-                justifyContent:"center",
-                alignItems:"center",
-                color:"black",
-                marginTop:"3%",
-                marginBottom:"10%",
-                marginLeft:"20%",
-                marginRight:"20%",
-                padding:"10px",
-                borderRadius:"15px",
-            }}>
-                
-                <Box sx={{
-                    width:"100%",
-                    
-                    backgroundColor:"white", 
-                    display:"flex",
-                    justifyContent:"center",
-                    alignItems:"center",
-                    color:"black",
-                    padding:"5px",
-                    borderRadius:"15px"
-                }}>
-                
-                    <h1 style={{fontFamily:"serif", color:"black"}}>Ask your doubts below!</h1>
-                </Box>
-            <Box sx={{
-                width:"100%",
-                height:"100%",
-                maxHeight:"85%",
-                backgroundColor:"white", 
-                display:"flex",
-                flexDirection:"column",
-                justifyContent:"center",
-                alignItems:"center",
-                color:"black",
-                margin:"10px",
-                padding:"10px",
-                borderRadius:"15px",
-            }}>
+            backgroundColor: "white",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            color: "black",
+            padding: "5px",
+            borderRadius: "15px",
+          }}
+        >
+          <h1 style={{ fontFamily: "serif", color: "black" }}>
+            Ask your doubts below!
+          </h1>
+        </Box>
+        <Box
+          sx={{
+            width: "100%",
+            height: "100%",
+            maxHeight: "85%",
+            backgroundColor: "white",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            color: "black",
+            margin: "10px",
+            padding: "10px",
+            borderRadius: "15px",
+          }}
+        >
+          <Box
+            sx={{
+              width: "100%",
+              height: "90%",
+              maxHeight: "90%",
+              backgroundColor: "white",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              color: "black",
+              marginBottom: "10px",
+              padding: "10px",
+              borderRadius: "15px",
+              border: "1px solid pink",
+            }}
+          >
+            <ScrollableChat message={messages} />
+          </Box>
 
-                <Box sx={{
-                width:"100%",
-                height:"90%",
-                maxHeight:"90%",
-                backgroundColor:"white", 
-                display:"flex",
-                justifyContent:"center",
-                alignItems:"center",
-                color:"black",
-                marginBottom:"10px",
-                padding:"10px",
-                borderRadius:"15px",
-                border:"1px solid pink",
-            }}>
-                    <ScrollableChat message = {messages} />
-                </Box>
-                
-                <Box
-                    sx={{
-                        width:"100%",
-                        border:"1px solid lightblue",
-                        borderRadius:"10px",
-                    }}
-                    >
-                    
-                        {/* //<EquationEditor> */}
-                        <TextField 
-                        id="text-bar"
-                        label="Ask Something..." 
-                        variant="outlined" 
-                        fullWidth
-                        value={question}
-                        onChange={handleChange}
-                        onKeyDown={sendChange}
-                    />
-                        {/* </EquationEditor> */}
+          <Box
+            sx={{
+              width: "100%",
+              border: "1px solid lightblue",
+              borderRadius: "10px",
+            }}
+          >
+            {/* //<EquationEditor> */}
+            <TextField
+              id="text-bar"
+              label="Ask Something..."
+              variant="outlined"
+              fullWidth
+              value={question}
+              onChange={handleChange}
+              onKeyDown={sendChange}
+            />
 
-                </Box>
+            <Box
+              sx={{
+                width: "100%",
+                height: "100px",
+                border: "1px solid lightblue",
+                borderRadius: "10px",
+              }}
+            >
+              {/* <Latex>{`Hello $x^2$ value $\\frac{1}{2}$ and $\\sum_{n=1}^{\infty} 2^{-n}$`}</Latex> */}
             </Box>
-            </Box>
-        </div>
-    )
+          </Box>
+        </Box>
+      </Box>
+    </div>
+  );
 }
--- virtual_ta_client/pages/index.js
+++ virtual_ta_client/pages/index.js
@@ -1,15 +1,24 @@
-import HomePage from '../components/HomePage';
-import WelcomeAppBar from '../components/AppBar';
-import Form from '../components/Form';
+import HomePage from "../components/HomePage";
+import WelcomeAppBar from "../components/AppBar";
+import Form from "../components/Form";
 
 export default function Home() {
-  
   return (
-    <div className='main'>
-      <WelcomeAppBar/>
+    <div className="main">
+      <WelcomeAppBar />
       <Form />
+      {/* <link
+        rel="stylesheet"
+        media="all"
+        href="https://guppy.js.org/build/guppy-default-osk.min.css"
+      />
+      <link
+        href="//cdnjs.cloudflare.com/ajax/libs/KaTeX/0.9.0/katex.min.css"
+        rel="stylesheet"
+      />
+      <script src="https://guppy.js.org//build/guppy_osk.min.js%22%3E" />
+      <script src="https://guppy.js.org//build/guppy.min.js%22%3E" /> */}
       {/* <HomePage sx={{zIndex:0}}/> */}
-      
     </div>
-  )
+  );
 }
