0f5da95636a99b615d55bd2471faf66c6fca8572
Madhur20 <59988556+Madhur20@users.noreply.github.com>
2022-11-03 12:14:09 -0500

    dep1

--- virtual_ta_client/components/ChatBox.js
+++ virtual_ta_client/components/ChatBox.js
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React from "react";
 import { Box } from "@mui/material";
 import TextField from "@mui/material/TextField";
 import Typography from "@mui/material";
