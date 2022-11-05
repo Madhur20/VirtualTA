dc20b99c403ef1943454ad42b48927575321c6b8
Madhur20 <59988556+Madhur20@users.noreply.github.com>
2022-11-05 15:21:59 -0500

    verD1

--- virtual_ta_client/components/ScrollableChat.js
+++ virtual_ta_client/components/ScrollableChat.js
@@ -1,35 +1,35 @@
-import React from 'react';
-import ScrollableFeed from 'react-scrollable-feed';
+import React from "react";
+import ScrollableFeed from "react-scrollable-feed";
 
-const ScrollableChat = ({message}) => {
-    console.log(message);
-    // const messages = e.target.value;
-    return (
-        
-        <ScrollableFeed >
-            {message && message.map((msg,ind) => (
-                <div style={{display:"flex", width:"100vh"}}>
-                    <span style={{
-                        backgroundColor: `${
-                            msg.id === "fromUser" ? "#BEE3F8" : "#B9F5D0"
-                        }`,
-                        borderRadius: "20px",
-                        padding:"5px 5px",
-                        maxWidth: "75%",
-                        marginLeft: `${
-                            msg.id === "fromUser" ? "auto" : "0px"
-                        }`,
-                        marginRight: `${
-                            msg.id === "fromUser" ? "0px" : "auto"
-                        }`,
-                        marginTop: "5px",
-                        overflowX:"initial"
-                    }}>{msg.msg}</span>
-                </div>
-            ))}
-            {/* <div style={{color:"red"}}>hello</div> */}
-        </ScrollableFeed>
-    )
-}
+const ScrollableChat = ({ message }) => {
+  console.log(message);
+  // const messages = e.target.value;
+  return (
+    <ScrollableFeed>
+      {message &&
+        message.map((msg, ind) => (
+          <div key={msg.id + ind} style={{ display: "flex", width: "100vh" }}>
+            <span
+              style={{
+                backgroundColor: `${
+                  msg.id === "fromUser" ? "#BEE3F8" : "#B9F5D0"
+                }`,
+                borderRadius: "20px",
+                padding: "5px 5px",
+                maxWidth: "75%",
+                marginLeft: `${msg.id === "fromUser" ? "auto" : "0px"}`,
+                marginRight: `${msg.id === "fromUser" ? "0px" : "auto"}`,
+                marginTop: "5px",
+                overflowX: "initial",
+              }}
+            >
+              {msg.msg}
+            </span>
+          </div>
+        ))}
+      {/* <div style={{color:"red"}}>hello</div> */}
+    </ScrollableFeed>
+  );
+};
 
 export default ScrollableChat;
